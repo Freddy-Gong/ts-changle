c68ba1a0cf45099577f5fea547b7abd524815c67
Freddy-Gong <21914103@zju.edu.cn>
2022-03-23 16:26:34 +0800

    length

--- easy-first-of-array/template.ts
+++ easy-first-of-array/template.ts
@@ -1,8 +1,8 @@
-type First<T extends any[]> = T extends [] ? never : T[0]
+//type First<T extends any[]> = T extends [] ? never : T[0]
 //第一种解法是判断 T是否是空数组 如果是空数组就返回never 不是空数组就直接返回T[0]
-type First<T extends any[]> = T["length"] extends 0 ? never : T[0]
+//type First<T extends any[]> = T["length"] extends 0 ? never : T[0]
 //第二种解法是判断数组的长度，如果长度等于0返回never，不为零则返回T的第一个元素
-type First<T extends any[]> = T[0] extends T[number] ? T[0] : never
+//type First<T extends any[]> = T[0] extends T[number] ? T[0] : never
 //第三种解法判断T[0]的类型和T[number]是否相同，T[number]返回元素组成的联合类型
 //空数组会返回never
 type First<T extends any[]> = T extends [infer First, ...infer Rest] ? First : never
